b4a44caa985bd138afcda6a6833770ed36d4360f
Valentin Di Domenica <vdidomenica@kretz.com.ar>
2024-01-30 12:57:50 -0300

    cambios en el home

--- portfolio/src/Components/Home/Home.jsx
+++ portfolio/src/Components/Home/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React from "react";
+import styles from "./Home.module.css";
+import image from "../../../public/perfil.png";
 const Home = () => {
   return (
-    <div>home</div>
-  )
-}
+    <div>
+      <div className={styles.container}>
+        <h1 className={styles.titulo}>hola, este es mi portfolio</h1>
+        <img src={image} alt="icono developer" className={styles.iconDev} />
+      </div>
+      <div className={styles.containerTech}>
+        <h1>Tecnologias:</h1>
+        <ul className={styles.list}>
+          <li>✅Redux</li>
+          <li>✅React</li>
+          <li>✅Node</li>
+          <li>✅Express</li>
+          <li>✅Javascript</li>
+          <li>✅SQL</li>
+          <li>✅Sequelize</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
