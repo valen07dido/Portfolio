fb02daadcad1b072c1c2ece6d4874b738eac5bdf
valentin di domenica <valendido69@gmail.com>
2024-01-31 22:31:12 -0300

    about done

--- portfolio/src/Components/Contact/Contact.jsx
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
+import styles from "./Contact.module.css";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const redes = [
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/valentin-di-domenica-a6186822a",
+      icon: <FaLinkedin />,
+    },
+    {
+      name: "GitHub",
+      url: "https://github.com/valen07dido",
+      icon: <FaGithub />,
+    },
+    {
+      name: "Email",
+      url: "mailto:valendido69@gmail.com",
+      icon: <FaEnvelope />,
+    },
+    {
+      name: "Teléfono",
+      url: "tel:+5493413164761",
+      icon: <FaPhone />,
+    },
+  ];
 
-export default Contact
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Redes:</h2>
+      <div className={styles.containerNet}>
+        {redes.map((red) => (
+          <div className={styles.network}>
+            <a href={red.url} className={styles.link}>
+              {red.icon}
+              <span className={styles.name}>{red.name}</span>
+            </a>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
