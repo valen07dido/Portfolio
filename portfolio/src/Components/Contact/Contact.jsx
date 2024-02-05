58cb8a1622ca72f2555d008805ef883ec7bf39bd
Valentin Di Domenica <vdidomenica@kretz.com.ar>
2024-02-05 16:27:37 -0300

    form done and project started

--- portfolio/src/Components/Contact/Contact.jsx
+++ portfolio/src/Components/Contact/Contact.jsx
@@ -1,29 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Contact.module.css";
 
 const Contact = () => {
+  const [data, setData] = useState({
+    name: '',
+    email: '',
+    message: ''
+  });
 
-
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData(prevData => ({ ...prevData, [name]: value }));
+  }
+  console.log(data)
   return (
     <div className={styles.container}>
       <div>
         <form action="" className={styles.form}>
           <h1>Contactame</h1>
           <div className={styles.inputGroup}>
-            <input type="text" name="" id="" required />
+            <input type="text" name="name" id="" required onChange={handleChange} value={data.name} />
             <label htmlFor="">nombre</label>
           </div>
           <div className={styles.inputGroup}>
-            <input type="text" name="" id="" required />
+            <input type="text" name="email" id="" required onChange={handleChange} value={data.email}/>
             <label htmlFor="">email</label>
           </div>
           <div className={styles.inputGroup}>
-            <textarea name="" id="" cols="77" rows="10" required></textarea>
+            <textarea name="message" id="" cols="77" rows="10" required onChange={handleChange} value={data.message}></textarea>
             <label htmlFor="">mensaje</label>
           </div>
         </form>
       </div>
-
     </div>
   );
 };
--- portfolio/src/Components/Projects/Projects.jsx
+++ portfolio/src/Components/Projects/Projects.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import pokeapi from "../../../public/pokeapi.gif";
+import rickAndMorty from "../../../public/rickAndMorty.gif";
+import hyperMegaRed from "../../../public/hyperMegaRed.svg";
+import styles from './Projects.module.css'
 const Projects = () => {
+  const project = [
+    {
+      name: "pi pokemon",
+      image: pokeapi,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rhoncus efficitur mauris, id pretium nisi aliquam eu. Suspendisse egestas magna a pulvinar porta. Vestibulum facilisis libero elit, et pulvinar sem facilisis a. Etiam malesuada tempor aliquet. Quisque rutrum scelerisque tempus. Pellentesque elit leo, dictum sit amet magna ornare, pulvinar venenatis mi. Ut et posuere leo. Mauris in sapien tristique, convallis magna in, blandit nunc. Aenean lacinia eros ut dictum lobortis.",
+    },
+    {
+      name: "rick and morty",
+      image: rickAndMorty,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rhoncus efficitur mauris, id pretium nisi aliquam eu. Suspendisse egestas magna a pulvinar porta. Vestibulum facilisis libero elit, et pulvinar sem facilisis a. Etiam malesuada tempor aliquet. Quisque rutrum scelerisque tempus. Pellentesque elit leo, dictum sit amet magna ornare, pulvinar venenatis mi. Ut et posuere leo. Mauris in sapien tristique, convallis magna in, blandit nunc. Aenean lacinia eros ut dictum lobortis.",
+    },
+    {
+      name: "hyper mega red",
+      image: hyperMegaRed,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque rhoncus efficitur mauris, id pretium nisi aliquam eu. Suspendisse egestas magna a pulvinar porta. Vestibulum facilisis libero elit, et pulvinar sem facilisis a. Etiam malesuada tempor aliquet. Quisque rutrum scelerisque tempus. Pellentesque elit leo, dictum sit amet magna ornare, pulvinar venenatis mi. Ut et posuere leo. Mauris in sapien tristique, convallis magna in, blandit nunc. Aenean lacinia eros ut dictum lobortis.",
+    },
+  ];
   return (
-    <div>Projects</div>
-  )
-}
+    <div className={styles.container}>
+      {project.map((pro) => {
+        return (
+          <div className={styles.card}>
+            <h1 className={styles.name}>{pro.name}</h1>
+            <img src={pro.image} alt={pro.name} className={styles.image}/>
+            <p className={styles.content}>{pro.description}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
