0b926d26d3d898917bdc37e2029a24fafae82a14
valentin di domenica <valendido69@gmail.com>
2024-03-12 22:23:59 -0300

    modal al entrar a un proyecto

--- Client/src/Components/Projects/Projects.jsx
+++ Client/src/Components/Projects/Projects.jsx
@@ -1,16 +1,28 @@
 import React, { useEffect, useState } from "react";
 import styles from "./Projects.module.css";
+
 const Projects = () => {
   const [project, setProject] = useState([]);
+  const [selectedProject, setSelectedProject] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:5000/projects")
       .then((response) => response.json())
-      .then((data) =>{
-         console.log(data)
-        setProject(data)})
+      .then((data) => {
+        console.log(data);
+        setProject(data);
+      })
       .catch((error) => console.log(error.message));
   }, []);
+
+  const handleProjectClick = (pro) => {
+    setSelectedProject(pro);
+  };
+
+  const handleClose = () => {
+    setSelectedProject(null);
+  };
+
   return (
     <div className={styles.container}>
       {!project || project.length === 0 ? (
@@ -20,12 +32,30 @@ const Projects = () => {
       ) : (
         project.map((pro) => {
           return (
-            <div className={styles.card} key={pro.name}>
+            <div
+              className={styles.card}
+              key={pro.name}
+              onClick={() => handleProjectClick(pro)}
+            >
               <h1 className={styles.name}>{pro.name}</h1>
               <img src={pro.image} alt={pro.name} className={styles.image} />
             </div>
           );
         })
+      )}
+
+      {selectedProject && (
+        <div className={styles.modal}>
+          <div className={styles.popup}>
+            <div>
+              <h1>{selectedProject.name}</h1>
+              <img src={selectedProject.image} alt={selectedProject.name} />
+            </div>
+
+            <p className={styles.content}>{selectedProject.description}</p>
+          </div>
+          <button onClick={handleClose}>Cerrar</button>
+        </div>
       )}
     </div>
   );
