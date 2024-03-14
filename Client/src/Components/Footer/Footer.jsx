0702eb98a4d8fa4f4802e08619e5637a082c0b62
valentin di domenica <valendido69@gmail.com>
2024-03-13 22:25:37 -0300

    modificaciones en el footer

--- Client/src/Components/Footer/Footer.jsx
+++ Client/src/Components/Footer/Footer.jsx
@@ -1,45 +1,55 @@
 import React from "react";
-import { FaLinkedin, FaGithub, FaEnvelope, FaPhone } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaEnvelope, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
 import styles from "./Footer.module.css";
+
 const Footer = () => {
-  const redes = [
+  const socialNetworks = [
     {
-      name: "LinkedIn",
+      name: "\u00A0LinkedIn",
       url: "https://www.linkedin.com/in/valentin-di-domenica-a6186822a",
       icon: <FaLinkedin />,
     },
     {
-      name: "GitHub",
+      name: "\u00A0GitHub",
       url: "https://github.com/valen07dido",
       icon: <FaGithub />,
     },
     {
-      name: "Email",
+      name: "\u00A0Email",
       url: "mailto:valendido69@gmail.com",
       icon: <FaEnvelope />,
     },
     {
-      name: "Teléfono",
+      name: "\u00A0Teléfono",
       url: "tel:+5493413164761",
       icon: <FaPhone />,
     },
   ];
+
+  const renderSocialNetworks = () => {
+    return socialNetworks.map((network) => (
+      <div className={styles.network} key={network.name}>
+        <a href={network.url} className={styles.link}>
+          {network.icon}{network.name}
+        </a>
+      </div>
+    ));
+  };
+
   return (
     <div className={styles.container}>
       <div>
         <h2 className={styles.title}>Redes sociales:</h2>
         <div className={styles.containerNet}>
-          {redes.map((red) => (
-            <div className={styles.network} key={red.name}>
-              <a href={red.url} className={styles.link}>
-                {red.icon}
-              </a>
-            </div>
-          ))}
+          {renderSocialNetworks()}
         </div>
       </div>
-        <a href="">esta seria la ubicacion</a>
-      
+      <div>
+        <h2 className={styles.title}>Ubicación:</h2>
+        <a href="https://maps.app.goo.gl/4KGVPtLEeRDfnsrZ9" className={styles.link}>
+          <FaMapMarkerAlt /> Alvear, Santa Fe, Argentina
+        </a>
+      </div>
     </div>
   );
 };
