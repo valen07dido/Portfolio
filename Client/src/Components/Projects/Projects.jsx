cf8d597b5a18294b6101b2b689e148835f0ee035
valen07dido <valendido69@gmail.com>
2024-05-03 08:52:53 -0300

    condicional por si el backend no se levanta

--- Client/src/Components/Projects/Projects.jsx
+++ Client/src/Components/Projects/Projects.jsx
@@ -4,7 +4,7 @@ const url = import.meta.env.VITE_URL_BACKEND;
 import { BiWorld } from "react-icons/bi";
 import ReactPlayer from "react-player";
 import { FaExternalLinkAlt, FaGithub } from "react-icons/fa";
-import loadingIcon from "../../../public/tadpole-black-36.svg"
+import loadingIcon from "../../../public/tadpole-black-36.svg";
 
 const Projects = () => {
   const [project, setProject] = useState([]);
@@ -34,37 +34,71 @@ const Projects = () => {
     <div className={styles.containerGlobal}>
       <h1 className={styles.titles}>Proyectos:</h1>
       <div className={styles.container}>
-        {!project || project.length === 0 ? (
-          <div>
-            <h1>no hay proyectos</h1>
-          </div>
-        ) : (
-          project.map((pro) => {
-            return (
-              <div
-                className={styles.card}
-                key={pro.name}
-                onClick={() => handleProjectClick(pro)}
-              >
-                <h1 className={styles.name}>{pro.name}</h1>
-                <img src={pro.image} alt={pro.name} className={styles.image} />
-              </div>
-            );
-          })
-        )}
+        {!project || project.length === 0
+          ? setProject([
+              {
+                name: "pi pokemon",
+                image:
+                  "https://res.cloudinary.com/dpa8t14c2/image/upload/v1711067700/portfolio/kzeokyqejpc8pvxm9vok.png",
+                url: "https://front-pokemon-iota.vercel.app/",
+                repository: "https://github.com/valen07dido/piPokeapi.git",
+                description:
+                  "el proyecto de pokemon se consume de una API ademas de unificar los datos de la API a la base de datos y poder crear nuevos personajes, el proyecto tiene busqueda por filtros. detalle de personaje con caracteristicas y un apartado para crear personajes, la navegacion por la pagina es muy sencilla e intuitiva para brindarle al cliente el confort al navegar",
+              },
+              {
+                name: "rick and morty",
+                image:
+                  "https://res.cloudinary.com/dpa8t14c2/image/upload/v1711067701/portfolio/cokifbk6euzce9wr9sxw.png",
+                url: "https://rick-and-morty-ashy-chi.vercel.app/",
+                repository: "https://github.com/valen07dido/rick_and_morty.git",
+                description:
+                  "El proyecto de Rick And Morty fue mi primer proyecto utilizando react para el Front-end y Express para el Back-end. el mismo se basa en una app donde vas buscando distintos personajes de la serie y los va mostrando en el home. la app maneja inicio de sesion, apartado de favoritos y filtrado de los mismos.",
+              },
+              {
+                name: "hyper mega red",
+                image:
+                  "https://res.cloudinary.com/dpa8t14c2/image/upload/v1710290095/portfolio/gwougo0pz1jbobnexmad.svg",
+                url: "https://pf-henry-sepia.vercel.app/",
+                repository: "https://github.com/gabiMaglia/PF_HENRY.git",
+                description:
+                  "La temática es un e-commerce con el control de stock y creación de los productos por parte del administrador. Además de eso, también tiene el ingreso y seguimiento de reparaciones a servicio técnico para los 3 perfiles (administrador, técnico y cliente), todo esto notificado mediante mail y/o whatsapp. Además, tiene estadísticas para que el administrador pueda informarse de todos los eventos producidos en la aplicación, entre otras funcionalidades que destacan de cualquier e-commerce corriente.",
+              },
+            ])
+          : project.map((pro) => {
+              return (
+                <div
+                  className={styles.card}
+                  key={pro.name}
+                  onClick={() => handleProjectClick(pro)}
+                >
+                  <h1 className={styles.name}>{pro.name}</h1>
+                  <img
+                    src={pro.image}
+                    alt={pro.name}
+                    className={styles.image}
+                  />
+                </div>
+              );
+            })}
 
         {selectedProject && (
           <div className={styles.modal}>
             <div className={styles.popup}>
               <div className={styles.title}>
-                <h1 className={styles.projectName}>{selectedProject.name}</h1> 
+                <h1 className={styles.projectName}>{selectedProject.name}</h1>
                 <button onClick={handleClose} className={styles.button}>
                   X
                 </button>
               </div>
               <div className={styles.container1}>
                 <div className={styles.header}>
-                  {!videoReady && <img src={loadingIcon} alt="Loading" className={styles.loading}/>}
+                  {!videoReady && (
+                    <img
+                      src={loadingIcon}
+                      alt="Loading"
+                      className={styles.loading}
+                    />
+                  )}
                   <ReactPlayer
                     url="https://vimeo.com/906000568?share=copy"
                     controls
